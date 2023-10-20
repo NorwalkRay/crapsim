db22cba09ed75785932ee114d8f6644c333ed87c
Raaid <raaidahmad@gmail.com>
2023-10-19 20:13:50 -0400

    Renamed file and added functionality

--- Craps_Sim_v2.py
+++ Craps_Sim_v2.py
@@ -65,9 +65,20 @@ class Hedge6_Strategy(Strategy):
             # This should not happen as craps only has points 4, 5, 6, 8, 9, and 10
             raise ValueError("Invalid point value")
 
-    def adjust_for_roll(self, bet_table, roll_result):
-        # Your logic here
-        pass
+    def adjust_for_roll(self, bet_table, die1, die2, roll_history):
+        roll_sum = die1 + die2
+        if roll_sum == 7:
+            #lose
+        elif
+        last_roll = roll_history[-1]
+        if sum(last_roll) == 7:  # Replace with your own win condition logic
+            # Progression logic, such as doubling the bet
+            pass_line_bet = bet_table.get_bet_amount('Pass Line')
+            new_pass_line_bet = pass_line_bet * 2  # Example: double
+            bet_table.add_bet('Pass Line', new_pass_line_bet)
+        else:
+            # Reset to base bet
+            bet_table.add_bet('Pass Line', 25)
 
 class BetTable:
     def __init__(self):
@@ -87,6 +98,9 @@ class BetTable:
             if bet['bet_name'] == bet_name:
                 return bet['bet_amount']
         return 0  # Return 0 if the bet_name is not found
+    
+    def get_total_bet(self):
+        return sum(bet['bet_amount'] for bet in self.table)
 
 def roll_dice():
     die1 = randint(1, 6)
@@ -99,17 +113,18 @@ def run_game(starting_bankroll, strategy, pnl_by_roll):
     bankroll = starting_bankroll
     bet_table = BetTable()
     point = None
-    strategy.initial_bets(strategy, bet_table)
+    roll_history = []
+    strategy.initial_bets(bet_table)
+    bankroll -= strategy.get_total_bet()
 
     # Come Out Roll
     die1, die2 = roll_dice()
     roll_sum = die1 + die2
+    roll_history.append((die1, die2)) # Update roll history
     if roll_sum in [7, 11]:
-        return bankroll + bet_table.get_bet_amount('Pass Line')
+        return bankroll + bet_table.get_bet_amount('Pass Line') * 2 
     elif roll_sum in [2, 3, 12]:
-        # You lose immediately
-        # Adjust bankroll and bet table
-        pass
+        return bankroll
     else:
         point = roll_sum
         strategy.point_set_bets(bet_table, point)
@@ -117,10 +132,21 @@ def run_game(starting_bankroll, strategy, pnl_by_roll):
     # Loop to hit the point or seven out
     while point:
         die1, die2 = roll_dice()
-        strategy.adjust_for_roll(bet_table, die1, die2)
-        # Logic to determine if point is hit or seven out
-        # Adjust bankroll and bet table accordingly
-        # Break loop if point is hit or seven out
+        roll_history.append((die1, die2)) # Update roll history
+        roll_sum = die1 + die2
+        
+        if roll_sum == point: # Point is hit
+            bankroll += bet_table.get_bet_amount('Pass Line')
+            return bankroll
+        elif roll_sum == 7:
+            # Seven out
+            bankroll -= bet_table.get_bet_amount('Point')  # Assuming you have a bet for 'Point' in bet_table
+            return bankroll
+        
+        strategy.adjust_for_roll(bet_table, die1, die2, roll_history)
+
+    # Optional: Adjust bankroll for other bets that might have won or lost
+    # bankroll += bet_table.some_other_bet_calculation()
 
     game_pnl = bankroll - starting_bankroll
     pnl_by_roll.append(game_pnl)
---
+++ test_sim.py
@@ -0,0 +1,18 @@
+import unittest
+from Craps_Sim_v2 import BetTable
+
+class TestBetTable(unittest.TestCase):
+
+    def test_add_bet(self):
+        table = BetTable()
+        table.add_bet('Pass Line', 25)
+        self.assertEqual(table.get_bet_amount('Pass Line'), 25)
+        
+    def test_total_amount_bet(self):
+        table = BetTable()
+        table.add_bet('Pass Line', 25)
+        table.add_bet('Odds', 50)
+        self.assertEqual(table.total_amount_bet(), 75)
+
+if __name__ == '__main__':
+    unittest.main()
